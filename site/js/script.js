03fefd423a51af2abd451502ec828c1378bca42d
joao matheus tenorio <joaomatheusteno@gmail.com>
2024-02-16 19:44:14 -0300

    atualizaçao
    mudança de cor e texto adicionado

--- site/js/script.js
+++ site/js/script.js
@@ -1,2 +1,2 @@
 let foot = document.querySelector('footer')
-foot.innerHTML = 'repositorio de João Matheus Tenorio da Silva'
+foot.innerHTML = 'repositorio de João Matheus Tenorio da Silva <br>entre em contato<br>WhatsApp ; +55 82 99664-7296<br>Instagram ; <a href="https://www.instagram/joaomteno?igsh=MTB)cWJnd3A4cXUwdw==" target = "_blank">joaomteno</a><br>&copy;Todos os direitos reservados - João Matheus Tenorio da Silva'
